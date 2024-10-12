61155da1b624c0b5399ea7d00f228e0f42676375
Ananya Maurya <maurya.ana@northeastern.edu>
2024-10-12 01:42:30 -0400

    Lab_4

---
+++ Lab_4/src/model/Product.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author anany
+ */
+public class Product {
+    private String name;
+    private int price;
+    private int id;
+
+     public static void setCount(int count) {
+        Product.count = count;
+    }
+    private ArrayList<Feature> features;
+    private static int count=0;
+    public Product(){
+        count++;
+        id=count;
+        features=new ArrayList<Feature>();
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ArrayList<Feature> getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(ArrayList<Feature> features) {
+        this.features = features;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+   
+    
+}
